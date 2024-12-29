50903f83651e820eab578f48fe64f50fcb6f4b30
Ramon Cunha <ramon.network@hotmail.com>
2024-12-28 22:58:13 -0300

    add network to docker and create callback endpoint

--- authentication/authorization-code-flow/src/index.ts
+++ authentication/authorization-code-flow/src/index.ts
@@ -10,14 +10,30 @@ app.get("/login", (req, res) => {
     scope: "openid",
   });
   const url = `http://localhost:8080/realms/myapp/protocol/openid-connect/auth?${loginParams.toString()}`;
-  console.log(url);
 
   res.redirect(url);
 });
 
-app.get("/callback", (req, res) => {
-  console.log(req.query);
-  res.send("ok")
+app.get("/callback", async (req, res) => {
+  const bodyParams = new URLSearchParams({
+    client_id: "myapp-client",
+    grant_type: "authorization_code",
+    code: req.query.code as string,
+    redirect_uri: "http://localhost:3000/callback",
+  });
+
+  const url =
+    "http://172.17.0.1:8080/realms/myapp/protocol/openid-connect/token";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: bodyParams.toString(),
+  });
+
+  res.json(await response.json());
 });
 
 app.listen(3000, () => {
