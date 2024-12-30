085461ec87e9a8f0b6f593ce3bb93c7da7f83e00
Ramon Cunha <ramon.network@hotmail.com>
2024-12-30 10:02:08 -0300

    start implicit flow auth

--- authentication/implicit-flow/src/App.tsx
+++ authentication/implicit-flow/src/App.tsx
@@ -1,9 +1,33 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Login } from "./Login";
+import { Callback } from "./Callback";
+import { Logout } from "./Logout";
+
+const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "logout",
+    element: <Logout />,
+  },
+  // {
+  //   path: "admin",
+  //   element: (
+  //     <PrivateRoute>
+  //       <Admin />
+  //     </PrivateRoute>
+  //   ),
+  // },
+  {
+    path: "callback",
+    element: <Callback />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1>Implicit Flow</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
